3278590f0970aafa0cd569906f02bee3e7d27bf0
rlm80 <rlemaigr@yahoo.fr>
2010-08-11 13:04:16 +0200

    wip

--- classes/gluedb/database.php
+++ classes/gluedb/database.php
@@ -14,6 +14,57 @@ abstract class GlueDB_Database extends PDO {
 	 */
 	static protected $instances = array();
 	
+	/*
+	 * Same as PDO::query if statement is a string. If statement is a data structure
+	 * representing an SQL query, it is first compiled into an SQL string before being
+	 * handed over to the PDO corresponding method.
+	 * 
+	 * @see PDO::query()
+	 */
+	public function query($statement) {
+		$sql = $this->compile($statement);
+		return parent::query($sql);
+	}
+	
+	/*
+	 * Same as PDO::prepare if statement is a string. If statement is a data structure
+	 * representing an SQL query, it is first compiled into an SQL string before being
+	 * handed over to the PDO corresponding method.
+	 * 
+	 * @see PDO::prepare()
+	 */
+	public function prepare($statement, $driver_options) {
+		$sql = $this->compile($statement);
+		return parent::query($sql);		
+	}
+	
+	/*
+	 * Same as PDO::exec if statement is a string. If statement is a data structure
+	 * representing an SQL query, it is first compiled into an SQL string before being
+	 * handed over to the PDO corresponding method.
+	 * 
+	 * @see PDO::exec()
+	 */
+	public function exec($statement) {
+		$sql = $this->compile($statement);
+		return parent::query($sql);		
+	}
+	
+	/**
+	 * Compiles a datastructure representing an SQL query into an SQL string.
+	 * 
+	 * @param mixed $statement
+	 * 
+	 * @return string
+	 */
+	protected function compile($statement) {
+		if (is_string($statement))
+			return $statement;
+		else {
+			// This is where the magic happens.
+		}
+	}
+	
 	/**
 	 * Lazy loads a database object, stores it in cache, and returns it.
 	 * 
