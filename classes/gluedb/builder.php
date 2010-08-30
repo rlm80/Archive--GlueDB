f65b91a3a6e807db1a56cdd906e3ec9d64bee521
rlm80 <rlemaigr@yahoo.fr>
2010-08-30 23:06:59 +0200

    wip

--- classes/gluedb/builder.php
+++ classes/gluedb/builder.php
@@ -2,96 +2,99 @@
 
 /**
  * Base builder class.
- * 
- * A builder is an object that provides a fluent interface to build an expression.
- * 
- * An expression is a sequence of components. Components can be atomic or they can be
- * nested expressions. Functions that allow the user to add atomic components return the
- * current builder. Functions that mark the start of a nested expression return a
- * child builder that may be of a different class from its parent, with its own set
- * of methods and rules. When the child builder has finished its work, it returns the
- * parent builder.
+ *
+ * A builder is an object that provides a fluent interface to build an expression. An expression is
+ * represented internally as an array of components. Components will be run one by one through the
+ * compiler and concatenated to produce the resulting SQL expression.
  *
  * @package    GlueDB
  * @author     Régis Lemaigre
  * @license    MIT
  */
 
+/*
+ Note :
+ 	Using a "nested" fluent API (builders that may return children builders, not always $this) seems the most natural
+ 	way to go about this, since boolean and joins expressions are nested in nature. But as tempting as it may be, it is
+ 	NOT the right way to go about this. It is too confusing when the query has to be built in several steps, instead of
+ 	just one long "sentence" :
+
+		// suppose addthis() returns a child builder :
+ 		$query1->addthis()->addthat();
+
+		// You might think this is the same as the above :
+		$query2->addthis();
+		$query2->addthat(); // But it does NOT work !
+
+		// It shoud be :
+		$childbuilder = $query2->addthis();
+		$childbuilder->addthat();
+
+	Just too confusing...and that's only the tip of the pile of problems that arise from it when things get more complex.
+ */
+
 abstract class GlueDB_Builder {
+	/**
+	 * @var GlueDB_Query The query that is the context of this builder.
+	 */
+	protected $query;
+
 	/**
 	 * @var array Components of current expression.
 	 */
 	protected $parts;
-		
-	/**
-	 * @var GlueDB_Builder Parent builder that gave birth to this builder.
-	 */
-	protected $parent;
-	
+
 	/**
 	 * Constructor.
-	 * 
-	 * @param GlueDB_Builder $parent
+	 *
+	 * @param GlueDB_Query $query
 	 */
-	public function __construct(GlueDB_Builder $parent = null) {
-		$this->parent = $parent;
+	public function __construct(GlueDB_Query $query, $forward = false) {
 		$this->parts = array();
+		$this->query = $query;
+		$this->forward = $forward;
 	}
-	
-	/**
-	 * Marks the end of the current expression and returns the parent builder so we
-	 * can keep building on the parent expression using fluent methods.
-	 * 
-	 * @return GlueDB_Builder
-	 */
-	public function end() {
-		return $this->parent;
-	}
-	
+
 	/**
 	 * Removes all expression components added so far.
-	 * 
+	 *
 	 * @return GlueDB_Builder
 	 */
 	public function reset() {
 		$this->parts = array();
 		return $this;
 	}
-	
+
 	/**
 	 * Whether or not current expression is empty.
-	 * 
+	 *
 	 * @return boolean
 	 */
 	public function isempty() {
 		return count($this->parts) === 0;
-	}	
-	
+	}
+
 	/**
 	 * Returns expression components.
-	 * 
+	 *
 	 * @return array
 	 */
 	public function parts() {
 		return $this->parts;
-	}	
-	
+	}
+
 	/**
-	 * A call to an unkown function ends the current expression and the call
-	 * is forwarded to the parent builder. 
-	 * 
+	 * Any call to an unkown function is forwarded to the query.
+	 *
 	 * @param string $name
 	 * @param array $arguments
-	 * 
+	 *
 	 * @return mixed|NULL
 	 */
 	public function __call($name, $arguments) {
-		// Is there a parent ?
-		if (isset($this->parent))
-			// Forwards call to parent builder if any :
+		if ($this->forward)
 			return call_user_func_array(array($this->end(), $name), $arguments);
 		else {
-			// No parent => trigger error :
 			$trace = debug_backtrace();
 	        trigger_error(
 				'Undefined method via __call(): ' . $name .
--- classes/gluedb/builder/boolean.php
+++ classes/gluedb/builder/boolean.php
@@ -13,26 +13,26 @@ class GlueDB_Builder_Boolean extends GlueDB_Builder {
 	 * Adds parts to the expression, surrounding them with parenthesis, and connecting
 	 * them to the expression with given connector. Connector is ignored if expression
 	 * is empty.
-	 * 
+	 *
 	 * @param array $parts
-	 * @param string $connector 
+	 * @param string $connector
 	 */
 	protected function add($parts, $connector = null) {
 		// Add connector :
 		if (isset($connector) && ! $this->isempty())
 			$this->parts[] = ' ' . $connector . ' ';
-			
+
 		// Add parts :
 		$this->parts[] = '(';
 		$this->parts = array_merge($this->parts, $parts);
 		$this->parts[] = ')';
-	}	
-		
+	}
+
 	/**
 	 * Use ->or() instead of this. Adds arguments to the expression, surrounding them
 	 * with parenthesis, and connecting them to the expression with 'OR' if the expression
 	 * isn't empty.
-	 * 
+	 *
 	 * @return GlueDB_Builder_Boolean
 	 */
 	public function _or() {
@@ -45,56 +45,51 @@ class GlueDB_Builder_Boolean extends GlueDB_Builder {
 	 * Use ->and() instead of this. Adds arguments to the expression, surrounding them
 	 * with parenthesis, and connecting them to the expression with 'AND' if the expression
 	 * isn't empty.
-	 * 
+	 *
 	 * @return GlueDB_Builder_Boolean
 	 */
 	public function _and() {
 		$args = func_get_args();
 		$this->add($args, 'AND');
-		return $this;		
+		return $this;
 	}
-	
+
 	/**
-	 * Starts a new boolean expression and initializes it with given arguments. The new
-	 * expression will be surrounded by with parenthesis, and connected to the expression
-	 * with 'OR' if the expression isn't empty. Returns the builder object for the
-	 * new expression.
-	 * 
+	 * Inserts a nested expression into the current expression. The parameter is initialized
+	 * with a builder that can be used at a later time to define the content of the nested
+	 * expression.
+	 *
 	 * @return GlueDB_Builder_Boolean
-	 */	
-	public function or_nested() {
-		// Init new builder :
-		$args = func_get_args();
-		$builder = new GlueDB_Builder_Boolean($this);
-		$builder->add($args);
-		
+	 */
+	public function orexpr(GlueDB_Builder_Boolean &$builder) {
+		// Init builder :
+		$builder = new GlueDB_Builder_Boolean($this->query);
+
 		// Add builder :
 		$this->add(array($builder), 'OR');
-		
-		return $builder;		
-	}	
+
+		return $this;
+	}
 
 	/**
 	 * Starts a new boolean expression and initializes it with given arguments. The new
 	 * expression will be surrounded by with parenthesis, and connected to the expression
 	 * with 'OR' if the expression isn't empty. Returns the builder object for the
 	 * new expression.
-	 * 
+	 *
 	 * @return GlueDB_Builder_Boolean
-	 */	
-	public function and_nested() {
-		// Init new builder :
-		$args = func_get_args();
-		$builder = new GlueDB_Builder_Boolean($this);
-		$builder->add($args);
-		
+	 */
+	public function andexpr(GlueDB_Builder_Boolean &$builder) {
+		// Init builder :
+		$builder = new GlueDB_Builder_Boolean($this->query);
+
 		// Add builder :
-		$this->add($builder, 'AND');
-		
-		return $builder;		
-	}		
-	
-	/* 
+		$this->add(array($builder), 'AND');
+
+		return $this;
+	}
+
+	/*
 	 * Redefined to setup aliases for _or() and _and(). Required because
 	 * keywords aren't valid function names in PHP.
 	 */
